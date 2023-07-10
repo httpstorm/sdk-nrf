dc4f2a9e4b2b8219223600a9382cc56ae2a55253
Georgi Valkov <gvalkov@gmail.com>
2023-07-10 07:06:00 +0300

    2023-07-10
    connectivity_bridge: fix malfunction by replacing the UART driver
    
    An improved UART driver for nRF52840 boards is provided which
    offers very low latency, simple to use API and is more reliable
    than the original driver. The new driver is integrated in the
    connectivity_bridge application, to address reliability issues
    with the original driver.
    
    https://devzone.nordicsemi.com/f/nordic-q-a/67372/connectivity_bridge-malfunctions-when-ble-client-disconnects-and-reconnects
    
    Fix malfunction in connectivity_bridge caused by the UART driver:
    * connectivity_bridge randomly stops forwarding data from UART
    to BLE and USB clients. The issue resolves on its own within 5
    minutes, or when the board is restarted. The issue usually
    appears a few minutes after boot. In one test scenario, we had
    a very active bi-directional communication between nRF9160 and
    a BLE device. In another test we were sending AT commands
    automatically from the BLE client, to monitor the modem status.
    In a third test, we were sending the AT commands manually over
    the USB terminal. During most of our tests, a USB terminal was
    also attached to monitor the communication. When the issue
    appears, both USB terminal and BLE client are affected and
    stop receiving data for about 5 minutes.
    
    This patch resolves the aforementioned issues, by offering an
    alternative UART miniport driver for nRF52840. The driver is
    well tested and reliable. EasyDMA is enabled by default.
    Optionally the driver can be switched to interrupt or polling.
    All connectivity bridge features and power saving are preserved.
    Board configuration is used e.g. enabled, pins, baudrate, and
    hardware flow control. The driver API is very simple and to use.
    It is designed to provide very low latency and efficient
    CPU usage.
    
    When CONFIG_NRFX_UARTE is enabled, connectivity_bridge uses the
    original UART driver. When disabled, the UART miniport is
    enabled as a fall-back. It is recommended to disable all
    CONFIG_NRFX_UARTE* and CONFIG_UART* features, see prj.conf.
    CONFIG_DYNAMIC_INTERRUPTS, and
    CONFIG_DYNAMIC_DIRECT_INTERRUPTS must be enabled.
    
    Added features for nRF9160dk: Thingy:91 like behaviour:
    Data received over UART0 and UART1 is sent over TX on the same
    UART: data received from nRF9160 is forwarded to VCOM1, as well
    as the BLE client. Data received from VCOM1 or BLE is forwarded
    to nRF9160. Pin configuration should be configured accordingly
    in the board files. UART power saving is disabled for this board,
    since nRF5240 is not aware when a USB terminal is attached to
    the board controller. UARTs are cross-wired: nRF9160 connects to
    UART0.RX and UART1.TX, while VCOM1 connects to
    UART0.TX and UART1.RX.
    
    Fix DMA.RX synchronisation
    Use an event under Zephyr
    
    TODO:
    While the suggested changes to connectivity_bridge are well tested
    and reliable, it would be best to integrate the UART miniport
    driver into Zephyr or make it an optional NRF component, which can
    be selected instead of the original driver. It is suggested to
    create an option CONFIG_UART_MINIPORT_ENABLE, that automatically
    enables and makes the driver available for any application.
    
    Signed-off-by: Georgi Valkov <gvalkov@gmail.com>

---
+++ applications/connectivity_bridge_g/src/uart/include/int.h
@@ -0,0 +1,65 @@
+// ***************************************************************************
+//
+// File:         int.h
+// Author(s):    Georgi Valkov (GV)
+// Project:      nano RTOS
+// Description:  interrupt services
+//
+// Copyright (C) 2021-2023 by httpstorm.com
+//                      Tselina str. 6, 4000 Plovdiv, Bulgaria
+//                      e-mail: gvalkov@gmail.com
+//
+// ***************************************************************************
+
+#ifndef _INT_H
+#define _INT_H
+
+// Include files *************************************************************
+
+#include <os_layer.h>
+
+
+// Macros ********************************************************************
+
+// Types *********************************************************************
+
+_BEGIN_CPLUSPLUS
+
+typedef uint32_t	int_state_t;
+
+typedef void (* fn_int_handler)(void);
+typedef void (* fn_int_handler2)(const void *);
+
+
+// Global Prototypes *********************************************************
+
+// Function prototypes *******************************************************
+
+// low level interrupt services
+void int_disable(void);
+void int_enable(void);
+
+int_state_t	int_disable_nested(void);
+void int_enable_nested(int_state_t int_state_old);
+
+int_state_t int_get_faultmask_or_primask(void);
+int_state_t int_get_faultmask(void);
+int_state_t int_get_primask(void);
+#define int_is_disabled int_get_primask
+int_state_t int_unavailable(void);
+int_state_t int_get_active(void);
+
+// interrupt services
+int int_source_disable(uint32_t int_number);
+int int_source_enable(uint32_t int_number);
+int int_source_configure(uint32_t int_number, uint8_t prio, uint8_t mode);
+
+int int_set_vector(uint32_t int_number, fn_int_handler int_handler);
+int int_set_vector2(uint32_t int_number, fn_int_handler2 int_handler);
+fn_int_handler int_get_vector(uint32_t int_number);
+fn_int_handler2 int_get_vector2(uint32_t int_number);
+const void * int_get_vector_arg(uint32_t int_number);
+
+_END_CPLUSPLUS
+
+#endif // _INT_H
---
+++ samples/nrf9160/gnss_thingy/src/uart/os/int.s
@@ -0,0 +1,105 @@
+// ***************************************************************************
+//
+// File:         int.s
+// Author(s):    Georgi Valkov (GV)
+// Project:      nano RTOS
+// Description:  interrupt services
+//
+// Copyright (C) 2021-2023 by httpstorm.com
+//                      Tselina str. 6, 4000 Plovdiv, Bulgaria
+//                      e-mail: gvalkov@gmail.com
+//
+// ***************************************************************************
+
+// Include files *************************************************************
+
+// Macro definitions *********************************************************
+
+// exported function in separate section
+	.macro _global_func func_name
+	.text
+	.code 16
+	.balign 2
+	.thumb_func
+	.global \func_name
+	.type \func_name,#function
+\func_name:
+	.endm
+
+// Types *********************************************************************
+
+// Global variables **********************************************************
+
+// Functions *****************************************************************
+
+	.text
+	.thumb
+	.balign 2
+	.syntax unified
+
+//# disable interrupts
+//# void int_disable(void)
+_global_func int_disable
+	cpsid i
+	bx lr
+
+//# enable interrupts
+//# void int_enable(void)
+_global_func int_enable
+	cpsie i
+	bx lr
+
+//# disable interrupts, calls can be nested
+//# return the previous state
+//# int_state_t int_disable_nested(void)
+_global_func int_disable_nested
+	mrs   r0, primask                   //# r0 = interrupts disabled flag
+	cpsid i
+	bx lr
+
+//# restore interrupts to int_state, calls can be nested
+//# void int_enable_nested(int_state_t int_state)
+_global_func int_enable_nested
+	msr   primask, r0                   //# interrupts disabled flag = r0
+	bx lr
+
+
+//# returns (faultmask | primask) faults or interrupts disabled
+//# tIntState int_get_faultmask_or_primask(void)
+_global_func int_get_faultmask_or_primask
+	mrs   r0, faultmask                 //# r0 = faults disabled flag
+	mrs   r1, primask                   //# r1 = interrupts disabled flag
+	orr   r0, r1                        //# r0 |= r1
+	bx lr
+
+//# returns faultmask which is 1 when faults are disabled
+//# tIntState int_get_faultmask(void)
+_global_func int_get_faultmask
+	mrs   r0, faultmask                 //# r0 = faults disabled flag
+	bx lr
+
+//# returns primask which is 1 when interrupts are disabled
+//# tIntState int_get_primask(void)
+_global_func int_get_primask
+	mrs   r0, primask                   //# r0 = interrupts disabled flag
+	bx lr
+
+//# returns (faultmask | primask | currently active interrupt number)
+//# faults or interrupts disabled or running as an interrupt handler
+//# int_state_t int_unavailable(void)
+_global_func int_unavailable
+	mrs   r0, faultmask                 //# r0 = faults disabled flag
+	mrs   r1, primask                   //# r1 = interrupts disabled flag
+	mrs   r2, ipsr                      //# r2 = ipsr  currently active ISR
+	orr   r0, r1                        //# r0 |= r1
+	orr   r0, r2                        //# r0 |= r2
+	bx lr
+
+//# returns the currently active interrupt number
+//# int_state_t int_get_active(void)
+_global_func int_get_active
+	mrs   r0, ipsr                      //# r0 = ipsr  currently active ISR
+	bx lr
+
+// End of int.s **************************************************************
+.end
